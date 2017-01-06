f0dc8151524c866dffb008d4f3a3a1a15a578cbb
Sorrel Brigman <sorrel.k@gmail.com>
2017-01-06 13:29:11 -0600

    added basic constructor functions

--- orchard.js
+++ orchard.js
@@ -1,12 +1,36 @@
 // Unit Tests
 
 // There should be a Plant function defined.
-// There should be a Tree function defined.
-// There should be a function named increaseHeight on the prototype of Plant.
-// There should be a function named decreaseHeight on the prototype of Plant.
+var Plant = function() {
 // There should be a height property on Plant.
-// There should be a function named grow on the prototype of Tree.
-// There should be a function named trim on the prototype of Tree.
-// When you invoke the trim method on Tree, the height of the Tree should be reduced by the integer value that is passed as an argument (e.g. PearTree.trim(2))
-// When you invoke the trim method on Tree, the values of the branches property should be decreased by one.
-// When you invoke the grow method on Tree, the height of the Tree should be increased by the integer value that is passed as an argument (e.g. PearTree.grow(2))
+  this.height;
+// There should be a function named increaseHeight on the prototype of Plant.
+  this.increaseHeight = function(growth) {
+    this.height = this.height + growth;
+  };
+// There should be a function named decreaseHeight on the prototype of Plant.
+  this.descreaseHeight = function(amount) {
+    this.height = this.height - amount;
+  };
+};
+// There should be a Tree function defined.
+var Tree = function() {
+  this.branches;
+
+  // There should be a function named grow on the prototype of Tree.
+  this.grow = function(amount) {
+  // When you invoke the grow method on Tree, the height of the Tree should be increased by the integer value that is passed as an argument (e.g. PearTree.grow(2))
+  this.height = this.height + amount;
+  };
+
+  // There should be a function named trim on the prototype of Tree.
+  this.trim = function(amount) {
+  // When you invoke the trim method on Tree, the height of the Tree should be reduced by the integer value that is passed as an argument (e.g. PearTree.trim(2))
+  this.height = this.height - amount;
+  // When you invoke the trim method on Tree, the values of the branches property should be decreased by one.
+  this.branches = this.branches - 1;
+  };
+
+}
+
+Tree.prototype = new Plant();
